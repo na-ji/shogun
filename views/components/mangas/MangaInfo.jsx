0810b554dd9db396009dfbf4f6d44b2436446d03
Naji Astier <naji.demolitionman@gmail.com>
2016-08-20 16:02:26 +0200

    Add external link to manga info

--- views/components/mangas/MangaInfo.jsx
+++ views/components/mangas/MangaInfo.jsx
@@ -1,8 +1,20 @@
 "use babel";
 import React from 'react';
 var _ = require('lodash');
+const {shell} = require('electron');
 
 class MangaInfo extends React.Component {
+    constructor() {
+        super();
+        // we bind 'this' to handleResize()
+        this.openExternal = this.openExternal.bind(this);
+    }
+
+    openExternal(e) {
+        e.preventDefault();
+        shell.openExternal(this.props.manga.url);
+    }
+
     render() {
         console.log(this.props.manga);
 
@@ -35,6 +47,10 @@ class MangaInfo extends React.Component {
                                 </tr>
                             );
                         })}
+                        <tr>
+                            <th>URL</th>
+                            <td><a target="_blank" href="#!" onClick={this.openExternal}><i className="fa fa-external-link"></i></a></td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
