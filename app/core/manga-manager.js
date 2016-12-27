9c59248678ecf99bb46bf5d211fded4284535933
Naji Astier <naji.demolitionman@gmail.com>
2016-12-27 14:38:50 +0100

    Fix manga manager

--- app/core/manga-manager.js
+++ app/core/manga-manager.js
@@ -1,18 +1,20 @@
 var Promise = require('promise');
 var CatalogManager = require('./catalog-manager');
 var Parser = require('./parser');
+var _ = require('lodash');
 
+/* global db */
 
 var MangaManager = {};
 
 MangaManager.getPopularManga = function (catalogName) {
     var catalog = CatalogManager.getCatalog(catalogName);
 
-    return new Promise(function (fulfill, reject) {
+    return new Promise(function (resolve, reject) {
         Parser.getPopularMangaList(catalog).then(function (mangas) {
             var promises = [];
 
-            _.forEach(mangas.mangas, function(manga) {
+            _.forEach(mangas.mangas, function (manga) {
                 promises.push(new Promise(function (resolve, reject) {
                     db.rel.find('manga', manga.id).then(function (doc) {
                         if (doc.mangas.length) {
@@ -27,7 +29,7 @@ MangaManager.getPopularManga = function (catalogName) {
                 }));
             });
 
-            fulfill({
+            resolve({
                 mangas: mangas.mangas,
                 promises: promises
             });
@@ -36,12 +38,12 @@ MangaManager.getPopularManga = function (catalogName) {
 };
 
 MangaManager.getMangaById = function (mangaId) {
-    return new Promise(function (fulfill, reject) {
+    return new Promise(function (resolve, reject) {
         let before = (new Date()).getTime();
         db.rel.find('manga', mangaId).then(function (doc) {
             console.log('getMangaById took %d ms', (new Date()).getTime() - before);
             if (doc.mangas.length) {
-                fulfill({
+                resolve({
                     manga: doc.mangas[0],
                     chapters: (doc.chapters ? doc.chapters : [])
                 });
@@ -52,30 +54,30 @@ MangaManager.getMangaById = function (mangaId) {
     });
 };
 
-MangaManager.toggleInLibrary = function(manga) {
+MangaManager.toggleInLibrary = function (manga) {
     manga.in_library = !manga.in_library;
     db.rel.save('manga', manga);
 };
 
-MangaManager.getChapterList = function(manga) {
+MangaManager.getChapterList = function (manga) {
     var catalog = CatalogManager.getCatalog(manga.catalog);
 
-    return new Promise(function (fulfill, reject) {
-        Parser.getChapterList(catalog, manga).then(function(chapters) {
+    return new Promise(function (resolve, reject) {
+        Parser.getChapterList(catalog, manga).then(function (chapters) {
             if (manga.in_library) {
                 // save chapter to DB
                 var chapterIds = [];
-                let done = _.after(chapters.length, function() {
+                let done = _.after(chapters.length, function () {
                     manga.chapters = _.union(manga.chapters, chapterIds);
-                    db.rel.save('manga', manga).catch(function(err) {
+                    db.rel.save('manga', manga).catch(function (err) {
                         console.log(err);
                     });
                 });
 
-                _.forEach(chapters, function(chapter) {
+                _.forEach(chapters, function (chapter) {
                     db.rel.find('chapter', chapter.id).then(function (doc) {
                         if (!doc.chapters.length) {
-                            db.rel.save('chapter', chapter).catch(function(err) {
+                            db.rel.save('chapter', chapter).catch(function (err) {
                                 console.log(err);
                             });
                         }
@@ -85,28 +87,28 @@ MangaManager.getChapterList = function(manga) {
                 });
             }
 
-            fulfill(chapters);
+            resolve(chapters);
         });
     });
 };
 
-MangaManager.getLibrary = function() {
-    return new Promise(function (fulfill, reject) {
+MangaManager.getLibrary = function () {
+    return new Promise(function (resolve, reject) {
         let before = (new Date()).getTime();
         db.query('manga_index/by_in_library', {
             key: true,
-            include_docs : true
+            include_docs: true
         }).then(function (response) {
             console.log('getLibrary took %d ms', (new Date()).getTime() - before);
             let mangas = [];
 
-            _.forEach(response.rows, function(row) {
+            _.forEach(response.rows, function (row) {
                 let manga = row.doc.data;
                 manga.id = row.id.replace('manga', '').replace(/^_\d+_/, '');
                 mangas.push(manga);
             });
 
-            fulfill(mangas);
+            resolve(mangas);
         });
     });
 };
