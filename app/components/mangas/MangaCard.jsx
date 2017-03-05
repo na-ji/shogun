f2d559568b00c63dec9ea10cc714fe2f1d6a2513
Naji Astier <naji.demolitionman@gmail.com>
2017-03-05 21:18:02 +0100

    Move manga card to less module

--- app/components/mangas/MangaCard.jsx
+++ app/components/mangas/MangaCard.jsx
@@ -1,16 +1,16 @@
-"use babel";
 import React from 'react';
+import styles from './mangaCard.less';
 
 class MangaCard extends React.Component {
-    render() {
+    render () {
         var inlineStyle = {
             backgroundImage: 'url(' + this.props.manga.thumbnail_url + ')'
         };
 
         return (
-            <div className="manga card">
-                <div className="manga-content" style={inlineStyle}>
-                    <div className="manga-title">
+            <div className={styles.manga + ' card'}>
+                <div className={styles.mangaContent} style={inlineStyle}>
+                    <div className={styles.mangaTitle}>
                         <p>{this.props.manga.title}</p>
                     </div>
                 </div>
