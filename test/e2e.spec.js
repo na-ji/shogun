1be7278a8e1183c9189a8fb7af3b88c9581eb55f
Naji Astier <naji.demolitionman@gmail.com>
2017-03-06 21:15:27 +0100

    Update e2e test

--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -2,16 +2,17 @@ import { Application } from 'spectron';
 import electronPath from 'electron';
 import path from 'path';
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
-
-const delay = time => new Promise(resolve => setTimeout(resolve, time));
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 
 describe('main window', function spec () {
     beforeAll(async () => {
         this.app = new Application({
             path: electronPath,
+            startTimeout: 10000,
+            waitTimeout: 10000,
             args: [path.join(__dirname, '..', 'app')]
         });
+
         return this.app.start();
     });
 
@@ -24,9 +25,10 @@ describe('main window', function spec () {
     it('should open window', async () => {
         const {client, browserWindow} = this.app;
 
-        await client.waitUntilWindowLoaded();
-        await delay(500);
-        const title = await browserWindow.getTitle();
-        expect(title).toBe('Shogun Manga Reader');
+        client.waitUntilWindowLoaded().getWindowCount().then(function (count) {
+            expect(count).toBe(1);
+            const title = browserWindow.getTitle();
+            expect(title).toBe('Shogun Manga Reader');
+        });
     });
 });
