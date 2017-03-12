02ab67ba57c64761c090d377cac29cc6e50113c3
Naji Astier <naji.demolitionman@gmail.com>
2017-03-12 11:07:53 +0100

    Refactor menu from upstream https://github.com/chentsulin/electron-react-boilerplate/commit/e607318c8a7ed0714327001166a1286f03c4cf7c

--- app/main.development.js
+++ app/main.development.js
@@ -1,7 +1,7 @@
-import { app, BrowserWindow, Menu } from 'electron';
+// @flow
+import { app, BrowserWindow } from 'electron';
+import MenuBuilder from './menu';
 
-let menu;
-let template;
 let mainWindow = null;
 
 if (process.env.NODE_ENV === 'production') {
@@ -52,6 +52,9 @@ app.on('ready', async () => {
     mainWindow.loadURL(`file://${__dirname}/app.html`);
 
     mainWindow.webContents.on('did-finish-load', () => {
+        if (!mainWindow) {
+            throw new Error('"mainWindow" is not defined');
+        }
         mainWindow.show();
         mainWindow.focus();
     });
@@ -60,171 +63,6 @@ app.on('ready', async () => {
         mainWindow = null;
     });
 
-    if (process.env.NODE_ENV === 'development') {
-        mainWindow.openDevTools();
-        mainWindow.webContents.on('context-menu', (e, props) => {
-            const { x, y } = props;
-
-            Menu.buildFromTemplate([{
-                label: 'Inspect element',
-                click () {
-                    mainWindow.inspectElement(x, y);
-                }
-            }]).popup(mainWindow);
-        });
-    }
-
-    if (process.platform === 'darwin') {
-        template = [{
-            label: 'Electron',
-            submenu: [{
-                label: 'About ElectronReact',
-                selector: 'orderFrontStandardAboutPanel:'
-            }, {
-                type: 'separator'
-            }, {
-                label: 'Services',
-                submenu: []
-            }, {
-                type: 'separator'
-            }, {
-                label: 'Hide ElectronReact',
-                accelerator: 'Command+H',
-                selector: 'hide:'
-            }, {
-                label: 'Hide Others',
-                accelerator: 'Command+Shift+H',
-                selector: 'hideOtherApplications:'
-            }, {
-                label: 'Show All',
-                selector: 'unhideAllApplications:'
-            }, {
-                type: 'separator'
-            }, {
-                label: 'Quit',
-                accelerator: 'Command+Q',
-                click () {
-                    app.quit();
-                }
-            }]
-        }, {
-            label: 'Edit',
-            submenu: [{
-                label: 'Undo',
-                accelerator: 'Command+Z',
-                selector: 'undo:'
-            }, {
-                label: 'Redo',
-                accelerator: 'Shift+Command+Z',
-                selector: 'redo:'
-            }, {
-                type: 'separator'
-            }, {
-                label: 'Cut',
-                accelerator: 'Command+X',
-                selector: 'cut:'
-            }, {
-                label: 'Copy',
-                accelerator: 'Command+C',
-                selector: 'copy:'
-            }, {
-                label: 'Paste',
-                accelerator: 'Command+V',
-                selector: 'paste:'
-            }, {
-                label: 'Select All',
-                accelerator: 'Command+A',
-                selector: 'selectAll:'
-            }]
-        }, {
-            label: 'View',
-            submenu: (process.env.NODE_ENV === 'development') ? [{
-                label: 'Reload',
-                accelerator: 'Command+R',
-                click () {
-                    mainWindow.webContents.reload();
-                }
-            }, {
-                label: 'Toggle Full Screen',
-                accelerator: 'Ctrl+Command+F',
-                click () {
-                    mainWindow.setFullScreen(!mainWindow.isFullScreen());
-                }
-            }, {
-                label: 'Toggle Developer Tools',
-                accelerator: 'Alt+Command+I',
-                click () {
-                    mainWindow.toggleDevTools();
-                }
-            }] : [{
-                label: 'Toggle Full Screen',
-                accelerator: 'Ctrl+Command+F',
-                click () {
-                    mainWindow.setFullScreen(!mainWindow.isFullScreen());
-                }
-            }]
-        }, {
-            label: 'Window',
-            submenu: [{
-                label: 'Minimize',
-                accelerator: 'Command+M',
-                selector: 'performMiniaturize:'
-            }, {
-                label: 'Close',
-                accelerator: 'Command+W',
-                selector: 'performClose:'
-            }, {
-                type: 'separator'
-            }, {
-                label: 'Bring All to Front',
-                selector: 'arrangeInFront:'
-            }]
-        }];
-
-        menu = Menu.buildFromTemplate(template);
-        Menu.setApplicationMenu(menu);
-    } else {
-        template = [{
-            label: '&File',
-            submenu: [{
-                label: '&Open',
-                accelerator: 'Ctrl+O'
-            }, {
-                label: '&Close',
-                accelerator: 'Ctrl+W',
-                click () {
-                    mainWindow.close();
-                }
-            }]
-        }, {
-            label: '&View',
-            submenu: (process.env.NODE_ENV === 'development') ? [{
-                label: '&Reload',
-                accelerator: 'Ctrl+R',
-                click () {
-                    mainWindow.webContents.reload();
-                }
-            }, {
-                label: 'Toggle &Full Screen',
-                accelerator: 'F11',
-                click () {
-                    mainWindow.setFullScreen(!mainWindow.isFullScreen());
-                }
-            }, {
-                label: 'Toggle &Developer Tools',
-                accelerator: 'Alt+Ctrl+I',
-                click () {
-                    mainWindow.toggleDevTools();
-                }
-            }] : [{
-                label: 'Toggle &Full Screen',
-                accelerator: 'F11',
-                click () {
-                    mainWindow.setFullScreen(!mainWindow.isFullScreen());
-                }
-            }]
-        }];
-        menu = Menu.buildFromTemplate(template);
-        mainWindow.setMenu(menu);
-    }
+    const menuBuilder = new MenuBuilder(mainWindow);
+    menuBuilder.buildMenu();
 });
